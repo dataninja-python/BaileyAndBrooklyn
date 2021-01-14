4b5c493aa7d13ebea203899480cd2924450353cb
brittanyleonard <Belah615@gmail.com>
2021-01-13 19:20:34 -0600

    added descriptions

--- models/products.js
+++ models/products.js
@@ -12,22 +12,22 @@ const Product = mongoose.model('Product', productSchema);
 
 module.exports = [
     {
-      name: 'FUTURE VOTER Onsie',
+      name: 'FUTURE VOTER Onesie',
       description: 'Baby onsie with simple text "FUTURE VOTER" available in multiple sizes/colors and can be customized',
       img: 'Bailey_And_Brooklyn_Shop/public/images/future_voter2.JPG',
-      price: 15.99,
+      price: 17.99,
       quantity: 10
     }, {
-      name: 'BLM Baby Onsie',
+      name: 'BLM Baby Onesie',
       description: 'BLM baby/toddler onsie. Proceeds will be donated to NAACP and ACLU',
       img: 'Bailey_And_Brooklyn_Shop/public/images/lgbt_pumpkins.JPG',
-      price: 15.99,
+      price: 17.99,
       qty: 10
    }, {
       name: 'I love my moms/dads',
       description: 'LGBTQ baby Onsie with pumpkin and customizable.',
       img: 'Bailey_And_Brooklyn_Shop/public/images/lgbt_pumpkins.JPG',
-      price: 15.99,
+      price: 17.99,
       qty: 10
    }, {
       name: 'Bear Onsie',
