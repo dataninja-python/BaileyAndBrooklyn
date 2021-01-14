bbdfa1825ac83ee8f0af4eb897f6131e58744926
brittanyleonard <Belah615@gmail.com>
2021-01-13 19:43:59 -0600

    product schema

--- models/products.js
+++ models/products.js
@@ -1,14 +1,3 @@
-const mongoose = require('mongoose');
-
-const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    img: {type: String, required: false},
-    description: {type: String, required: false},
-    price: {type: Number, required: true},
-    quantity: {type: Number, required: true}
-});
-
-const Product = mongoose.model('Product', productSchema);
 
 module.exports = [
     {
