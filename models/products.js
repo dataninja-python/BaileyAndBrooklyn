17e02e2171f70fceaef641e971dc2ba704f12837
brittanyleonard <Belah615@gmail.com>
2021-01-13 19:17:26 -0600

    seed images added

---
+++ models/products.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    img: {type: String, required: false},
+    description: {type: String, required: false},
+    price: {type: Number, required: true},
+    quantity: {type: Number, required: true}
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = [
+    {
+      name: 'FUTURE VOTER Onsie',
+      description: 'Baby onsie with simple text "FUTURE VOTER" available in multiple sizes/colors and can be customized',
+      img: 'Bailey_And_Brooklyn_Shop/public/images/future_voter2.JPG',
+      price: 15.99,
+      quantity: 10
+    }, {
+      name: 'BLM Baby Onsie',
+      description: 'BLM baby/toddler onsie. Proceeds will be donated to NAACP and ACLU',
+      img: 'Bailey_And_Brooklyn_Shop/public/images/lgbt_pumpkins.JPG',
+      price: 15.99,
+      qty: 10
+   }, {
+      name: 'I love my moms/dads',
+      description: 'LGBTQ baby Onsie with pumpkin and customizable.',
+      img: 'Bailey_And_Brooklyn_Shop/public/images/lgbt_pumpkins.JPG',
+      price: 15.99,
+      qty: 10
+   }, {
+      name: 'Bear Onsie',
+      description: 'Simple cute onsie. Black bear sillouette on a white onsie (customizable colors)',
+      img: 'Bailey_And_Brooklyn_Shop/public/images/bear_onsie2.JPG',
+      price: 17.99,
+      quantity: 10
+   }, {
+      name: 'All Hallows Eve Onesie',
+      description: 'Baby onsie with glitter design All Hollows Eve in pumpkin. Display image is in gold glitter but color can also be in black or purple. Can be customized/personalized.',
+      img: '/images/all-hallows-eve.jpg',
+      price: 17.99,
+      quantity: 10
+    }
+]
